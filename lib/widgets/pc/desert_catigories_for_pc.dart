c31571181e1a5060985434c217f37c81863ee34c
Young-dog <maxim_terekhin@bk.ru>
2023-03-16 13:55:43 +0300

    Add desert

--- lib/widgets/pc/desert_catigories_for_pc.dart
+++ lib/widgets/pc/desert_catigories_for_pc.dart
@@ -1,23 +1,22 @@
-import 'package:flutter/material.dart';
 import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
 import 'package:image_picker_web/image_picker_web.dart';
 import 'package:firebase_storage/firebase_storage.dart' as fires;
 
 import '../../links.dart';
 
-import 'add_desert.dart';
+class AddDesert extends StatefulWidget {
+  static const String id = "add_desert";
 
-class DesertCategoriesForPC extends StatefulWidget {
-  const DesertCategoriesForPC({Key? key}) : super(key: key);
+  const AddDesert({Key? key}) : super(key: key);
 
   @override
-  State<DesertCategoriesForPC> createState() => _DesertCategoriesForPCState();
+  State<AddDesert> createState() => _AddDesertState();
 }
 
-class _DesertCategoriesForPCState extends State<DesertCategoriesForPC> {
-  double width = 0;
-  double height = 0;
-  bool isAnnimated = true;
+class _AddDesertState extends State<AddDesert> {
+  final _formKey = GlobalKey<FormState>();
 
   String _name = "";
   late double _price;
@@ -62,123 +61,93 @@ class _DesertCategoriesForPCState extends State<DesertCategoriesForPC> {
 
   @override
   Widget build(BuildContext context) {
-
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height,
-      child: Column(
-        children: [
-          Text("Категории"),
-          Container(
-            width: 5,
-            height: 5,
-            child: GridView.builder(itemCount: 0,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 4,
-              mainAxisSpacing: 6,
-              crossAxisSpacing: 6,
-            ), itemBuilder: (context, index) {
-              return Container();
-            }),
-          ),
-          AnimatedContainer(
-            width: width,
-            height: height,
-            color: Colors.yellow,
-            duration: Duration(milliseconds: 400),
-            child:Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  child: FutureBuilder(
-                    future: getImage(),
-                    builder: (context, snapshot) {
-                      return imageFile == null ? MaterialButton(
-                        onPressed: () {
-                          getImage();
-                        },
-                        child: Text("Загрузить изображение"),
-                      ) : Expanded(
+    return Scaffold(
+      body: Form(
+        key: _formKey,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: backColor.withOpacity(0.1),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                child: FutureBuilder(
+                  future: getImage(),
+                  builder: (context, snapshot) {
+                    return imageFile == null ? MaterialButton(
+                      onPressed: () {
+                        getImage();
+                      },
+                      child: Text("Загрузить изображение"),
+                    ) : Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(50.0),
                         child: Image.memory(imageFile!),
-                      );
+                      ),
+                    );
 
-                    },
-                  ),
-                ),
-                Text("Название", style: italic),
-                SizedBox(
-                  height: 35,
-                ),
-                //name
-                TextFormField(
-                  decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black, width: 1.0),
-                          borderRadius: BorderRadius.circular(35))),
-                  textInputAction: TextInputAction.done,
-                  onFieldSubmitted: (_) {
-                    FocusScope.of(context).requestFocus(_priceFocusNode);
-                  },
-                  onChanged: (value) {
-                    _name = value;
-                  },
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Необходимо ввести название десерта!";
-                    }
-                    return null;
                   },
                 ),
-                SizedBox(
-                  height: 35,
-                ),
-                //price
-                Text("Цена", style: italic),
-                TextFormField(
-                  focusNode: _priceFocusNode,
-                  decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black, width: 1.0),
-                          borderRadius: BorderRadius.circular(35))),
-                  textInputAction: TextInputAction.done,
-                  onFieldSubmitted: (_) {
-                    _submit(imageFile: imageFile!);
-                  },
-                  onChanged: (value) {
-                    _price = double.parse(value);
-                  },
-                  validator: (value) {
-                    if (value == null) {
-                      return "Необходимо ввести цену продукта";
-                    }
-                    return null;
-                  },
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text("Загрузить"),
-                ),
-              ],
-            ),
+              ),
+
+              Text("Название", style: italic),
+              SizedBox(
+                height: 35,
+              ),
+              //name
+              TextFormField(
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black, width: 1.0),
+                        borderRadius: BorderRadius.circular(35))),
+                textInputAction: TextInputAction.done,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                onChanged: (value) {
+                  _name = value;
+                },
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Необходимо ввести название десерта!";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              //price
+              Text("Цена", style: italic),
+              TextFormField(
+                focusNode: _priceFocusNode,
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.black, width: 1.0),
+                        borderRadius: BorderRadius.circular(35))),
+                textInputAction: TextInputAction.done,
+                onFieldSubmitted: (_) {
+                  _submit(imageFile: imageFile!);
+                },
+                onChanged: (value) {
+                  _price = double.parse(value);
+                },
+                validator: (value) {
+                  if (value == null) {
+                    return "Необходимо ввести цену продукта";
+                  }
+                  return null;
+                },
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text("Загрузить"),
+              ),
+            ],
           ),
-          SizedBox(height: 10,),
-          TextButton(onPressed: () {
-            if (isAnnimated == false) {
-              setState(() {
-                width = 600;
-                height = 800;
-              });
-            } else {
-              setState(() {
-                width = 0;
-                height = 0;
-              });
-            }
-            isAnnimated = !isAnnimated;
-          }, child: Text("Animation"))
-
-        ],
+        ),
       ),
     );
   }
